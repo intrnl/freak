3ec9ca1f1621930700e69877dc715e3a543883e8
intrnl <intrnl28+git@gmail.com>
2021-10-15 16:52:23 +0700

    remove alias

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export { render } from './render.js';
 
 export { createElement, cloneElement, jsx, jsxs } from './element.js';
-export { createElement as h } from './element.js';
 
 export { Provider, createContext } from './context.js';
 export { Fragment } from './fragment.js';
